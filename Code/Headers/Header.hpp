f762c68f1f28fb7168a1596da8368055b523db2b
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2026-03-15 14:32:12 +0100

    adding header and struct rules

---
+++ Code/Headers/Header.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+typedef struct  s_rules{
+    unsigned long                       width;
+    unsigned long                       height;
+    unsigned long                       walkOn;
+    unsigned long                       unaccessible;
+    unsigned long                       walls;
+    unsigned long                       doors;
+    unsigned long                       chests;
+    unsigned long                       ennemies;
+    unsigned long                       bosses;
+    unsigned long                       pnjs;
+    unsigned long                       events;
+} t_rules;
--- Code/Headers/Map.hpp
+++ Code/Headers/Map.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include "DungeonManager.hpp"
-
+#include "Header.hpp"
 
 class Map{
     private:
@@ -8,32 +8,11 @@ class Map{
         const std::string                   name;
         size_t                              id;
         std::vector<std::string>            content;
-        unsigned long                       width;
-        unsigned long                       height;
-        unsigned long                       walkOn;
-        unsigned long                       unaccessible;
-        unsigned long                       walls;
-        unsigned long                       doors;
-        unsigned long                       chests;
-        unsigned long                       ennemies;
-        unsigned long                       bosses;
-        unsigned long                       pnjs;
-        unsigned long                       events;
-        
+        t_rules                             rules;
     public:
-        const std::string                   &getPath() const;
-        const std::string                   &getName() const;
-        const size_t                        &getId() const;
-        const std::vector<std::string>      &getContent() const;
-        const unsigned long                 &getWidth() const;
-        const unsigned long                 &getHeight() const;
-        const unsigned long                 &getWalkOn() const;
-        const unsigned long                 &getUnaccessible() const;
-        const unsigned long                 &getWalls() const;
-        const unsigned long                 &getDoors() const;
-        const unsigned long                 &getChests() const;
-        const unsigned long                 &getEnnemies() const;
-        const unsigned long                 &getBosses() const;
-        const unsigned long                 &getPnjs() const;
-        const unsigned long                 &getEvents() const;
+        const std::string                   &getPath();
+        const std::string                   &getName();
+        const size_t                        &getId();
+        const std::vector<std::string>      &getContent();
+        const t_rules                       &getRules();
 };
