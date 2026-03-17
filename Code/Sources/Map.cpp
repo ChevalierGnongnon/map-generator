d690ff347f1aa091d4dc40a4860337dc45ee7731
chhoflac <chhoflac@student.42.fr>
2026-03-17 09:54:15 +0100

    getters on map.cpp

---
+++ Code/Sources/Map.cpp
@@ -0,0 +1,35 @@
+#include "Header.hpp"
+
+Map::Map(t_rules &rules, std::vector<Section> &sections){
+
+}
+Map::Map(const Map &map){
+	
+}
+Map::~Map(){
+	
+}
+
+const std::string                   &Map::getPath() const{
+	return (this->path);
+}
+const std::string                   &Map::getName() const{
+	return (this->name);
+}
+const size_t                        &Map::getId() const{
+	return (this->id);
+}
+const std::vector<std::string>      &Map::getContent() const{
+	return (this->content);
+}
+const t_rules                       &Map::getRules() const{
+	return (this->rules);
+}
+
+
+void								Map::build(){
+	
+}
+void								Map::writeResult(){
+	
+}
